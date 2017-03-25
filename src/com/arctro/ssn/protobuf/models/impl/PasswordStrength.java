1c5508762bcb229c3aa1d6c044dc15490acc9e59
Ben McLean <benmclean007@gmail.com>
2017-03-25 20:25:13 +1100

    Added PasswordStrength message

---
+++ src/com/arctro/ssn/protobuf/models/impl/PasswordStrength.java
@@ -0,0 +1,12 @@
+package com.arctro.ssn.protobuf.models.impl;
+
+import com.arctro.ssn.protobuf.Protobuf;
+import com.arctro.ssn.protobuf.models.IProtobufWrapper;
+
+public class PasswordStrength extends IProtobufWrapper<Protobuf.PasswordStrength>{
+
+	public PasswordStrength(com.arctro.ssn.protobuf.Protobuf.PasswordStrength base) {
+		super(base);
+	}
+
+}
